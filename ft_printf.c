7dc046407e620f65d8ba105626fdd0244980e9b5
molesen <molesen@student.hive.fi>
2022-03-14 15:38:20 +0200

    added color through {}

--- ft_printf.c
+++ ft_printf.c
@@ -35,6 +35,44 @@ static void	ft_initialize_flag(t_flag *flag, int first)
 	}
 }
 
+static int	ft_color_check(const char *str, int len)
+{
+	if (ft_strncmp(&str[1], "reset", len) == 0)
+		ft_putstr("\033[0m");
+	else if (ft_strncmp(&str[1], "bold", len) == 0)
+		ft_putstr("\033[1m");
+	else if (ft_strncmp(&str[1], "red", len) == 0)
+		ft_putstr("\033[31m");
+	else if (ft_strncmp(&str[1], "green", len) == 0)
+		ft_putstr("\033[32m");
+	else if (ft_strncmp(&str[1], "yellow", len) == 0)
+		ft_putstr("\033[33m");
+	else if (ft_strncmp(&str[1], "blue", len) == 0)
+		ft_putstr("\033[34m");
+	else if (ft_strncmp(&str[1], "purple", len) == 0)
+		ft_putstr("\033[35m");
+	else if (ft_strncmp(&str[1], "cyan", len) == 0)
+		ft_putstr("\033[36m");
+	else if (ft_strncmp(&str[1], "white", len) == 0)
+		ft_putstr("\033[37m");
+	else if (ft_strncmp(&str[1], "uncolor", len) == 0)
+		ft_putstr("\033[39m");
+	else
+		return (FALSE);
+	return (TRUE);
+}
+
+static int	ft_color_print(const char *str, t_flag *flag)
+{
+	int	len;
+
+	len = ft_strlen_stop((char *)&str[1], '}');
+	if (ft_color_check(str, len) == FALSE)
+		return (FALSE);
+	flag->i += len + 1;
+	return (TRUE);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	arg;
@@ -50,7 +88,15 @@ int	ft_printf(const char *format, ...)
 		while (format[flag->i] != '\0')
 		{
 			if (format[flag->i] != '%')
-				ft_putchar(format[flag->i]);
+			{
+				if (format[flag->i] == '{')
+				{
+					if (ft_color_print(&format[flag->i], flag) == FALSE)
+						ft_putchar(format[flag->i]);
+				}
+				else
+					ft_putchar(format[flag->i]);
+			}
 			else
 			{
 				flag->i++;
